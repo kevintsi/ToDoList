851b4919ec5a9b773a967e252d63c7914c8157db
Kévin TSI <kevtsiii@gmail.com>
2023-02-01 11:50:37 +0100

    Reorganisation code css

--- src/components/ProgressBar/ProgramBar.js
+++ src/components/ProgressBar/ProgramBar.js
@@ -3,7 +3,7 @@ import "./ProgressBar.css"
 const ProgressBar = ({ barStyle: { width, color } }) => {
     return (
         <div className="progress-bar-container ">
-            <div style={{ width: width + "%", borderRadius: "10px", textAlign: "center", backgroundColor: color, transition: "width 0.5s" }}></div>
+            <div style={{ width: width + "%", backgroundColor: color }}></div>
         </div>
     )
 }
